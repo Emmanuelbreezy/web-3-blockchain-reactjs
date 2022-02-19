5e2a606482c7e3a41fa43eed81a9cf7f481d688a
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2022-02-19 03:38:26 +0100

    first commit

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className="flex justify-center items-center py-3">
+       <div className="animate-spin rounded-full w-32 h-32 border-b-2 border-red-700" />
+    </div>
   )
 }
 
